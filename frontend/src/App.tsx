6bd3c5bc761662ed52793cf3e4769b022fe6c978
Joao Vitor Silveira <jjoaovvitorsilveira87@gmail.com>
2026-05-29 10:59:10 -0300

    initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+
+export default function App() {
+  return (
+    <div className="bg-zinc-900 min-h-screen">
+      <h1 className="text-3xl text-center text-white py-10">test</h1>  
+    </div>
+
+  )
+}
