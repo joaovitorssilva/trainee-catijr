caa65b93ed9bb18990276c72b9eec9b757ef6711
Joao Vitor Silveira <jjoaovvitorsilveira87@gmail.com>
2026-07-21 20:25:53 -0300

    feat(ui): Profile page updated

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
-import { getUserFollowers } from "@/api";
+import { getUserFollowers, getUserPlaylists } from "@/api";
 import ProfileHeader from "@/components/ProfileContent/ProfileHeader";
 import TopArtists from "@/components/ProfileContent/TopArtists";
-// import TopTracks from "@/components/ProfileContent/TopTracks";
+import FollowersSection from "@/components/ProfileContent/FollowersSection";
+import TopTracks from "@/components/ProfileContent/TopTracks";
+import PlaylistSection from "@/components/MainContent/PlaylistSection/PlaylistSection";
+import type { PlaylistNoMusicDTO } from "@/types/index.types";
 
 export default function ProfilePage() {
   const [user, setUser] = useState<string[]>([])
+  const [playlists, setPlaylists] = useState<PlaylistNoMusicDTO[]>([])
 
   useEffect(() => {
     getUserFollowers().then(setUser)
+    getUserPlaylists().then(setPlaylists)
   }, [])
 
   return (
-    <div className="flex flex-col gap-3 bg-home-bg-gradient-variant pb-10 rounded-lg ">
+    <div className="flex flex-col gap-8 bg-home-bg-gradient-variant pb-10 rounded-lg ">
       <ProfileHeader
         followers={String(user.length)}
+        playlistCount={String(playlists.length)}
       />
-      <div className="flex flex-col gap-6 px-4">
+      <div className="flex flex-col gap-6 px-2 md:px-4">
         <TopArtists />
-        {/* <TopTracks /> */}
+        <TopTracks />
+        <PlaylistSection/>
+        <FollowersSection/>
       </div>
     </div>
   )
